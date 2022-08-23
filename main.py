c311c4f785e3ba560db485e3d4d3052b0abeed41
Worthyzz <48038868+Worthyzz@users.noreply.github.com>
2022-08-23 19:52:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ data = {"jintian":{"value":jintian, "color":get_random_color()},
         "love_days":{"value":get_count(), "color":get_random_color()},
         "birthday_left":{"value":get_birthday(), "color":get_random_color()},
         "birthday_left2":{"value":get_birthday2(), "color":get_random_color()},
-        "words":{"value":get_words(), "color":get_random_color()}},
+        "words":{"value":get_words(), "color":get_random_color()},
         "lucky":{"value":lucky(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 res2 = wm.send_template(user_id2, template_id, data)
